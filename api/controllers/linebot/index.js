783868451ef59537e99661a30a532aacd325acba
poruruba <44470781+poruruba@users.noreply.github.com>
2020-10-04 22:58:55 +0900

    Update index.js

--- api/controllers/linebot/index.js
+++ api/controllers/linebot/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const IMAGE_URL_BASE = "https://garden.poruru.site:10443/linebot_image/";
-const AUDIO_URL_BASE = "https://garden.poruru.site:10443/gamebook/audio/";
+const IMAGE_URL_BASE = "【サーバのURL】/linebot_image/";
+const AUDIO_URL_BASE = "【サーバのURL】/gamebook/audio/";
 
 const line = require('@line/bot-sdk');
 const mm = require('music-metadata');
