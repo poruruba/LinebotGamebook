75decc4514b103b8682adfe95bc641eb5cbca5ec
poruruba <44470781+poruruba@users.noreply.github.com>
2020-10-04 23:00:37 +0900

    Update index.js

--- api/controllers/linebot/index.js
+++ api/controllers/linebot/index.js
@@ -205,11 +205,251 @@ function check_condition(items, have, nothave){
   return condition;
 }
 
+async function process_scenario(event, client, scenario, status){
+  // 現在のシーンを取得
+  var scene = scenario.scene.find(item => item.id == status.scene );
+  if( !scene )
+    throw "scene not found";
+
+  // ターン番号をインクリメント
+  status.turn++;
+
+  var messages = [];
+
+  // 獲得アイテムの処理
+  if(scene.acquire && scene.acquire.length > 0){
+    scene.acquire.forEach(item => add_item(status.items, item));
+    var message = {
+      type: "text",
+      text: scene.acquire.join('、') + "を手に入れた"
+    }
+    messages.push(message);
+  }
+
+  // ロストアイテムの処理
+  if( scene.lost && scene.lost.length > 0){
+    scene.lost.forEach(item => remove_item(status.items, item));
+    var message = {
+      type: "text",
+      text: scene.lost.join('、') + "を失った"
+    };
+    messages.push(message);
+  }
+
+  // メインダイアログ
+  var flex = {
+    type: "flex",
+    altText: scene.text,
+    contents: {
+      type: "bubble",
+      size: "kilo",
+      body: {
+        type: "box",
+        layout: "vertical",
+        contents: [],
+      },
+      footer:{
+        type: "box",
+        layout: "vertical",
+        contents:[],
+        flex: 0
+      }
+    }
+  };
+
+  if( scene.image && scene.image.background ){
+    // 画像が指定されていた場合
+    var image_url = IMAGE_URL_BASE + scene.image.background;
+    // 画像合成が指定されていた場合
+    if(scene.image.composite ){
+      scene.image.composite.forEach( select => {
+        if( !select.name )
+          return;
+
+        // アイテムの所持・非所持確認
+        var condition = check_condition(status.items, select.have, select.nothave );
+        if( !condition )
+          return;
+
+        // 合成画像の指定追加
+        image_url += '-' + select.name;
+        if( select.position != undefined )
+          image_url += '_' + select.position;
+      });
+    }
+    flex.contents.hero = {
+      type: "image",
+      url: encodeURI(image_url),
+      size: "full",
+      aspectRatio: "20:13",
+      aspectMode: "fit"
+    };
+  }
+
+  if( scene.title ){
+    // タイトルが指定されていた場合
+    flex.contents.body.contents.push({
+      type: "text",
+      wrap: true,
+      text: scene.title,
+      weight: "bold",
+      size: "md"
+    });
+  }
+
+  // テキストの設定
+  flex.contents.body.contents.push({
+    type: "text",
+    wrap: true,
+    size: "sm",
+    text: scene.text
+  });
+
+  // 次の選択肢
+  if( scene.selection ){
+    scene.selection.forEach( select =>{
+      if( !select.id )
+        return;
+        
+      // アイテムの所持・非所持確認
+      var condition = check_condition(status.items, select.have, select.nothave );
+      if( !condition )
+        return;
+
+      // 選択肢の追加
+      var type = "scene";
+      if( select.type )
+        type = select.type;
+
+        flex.contents.footer.contents.push({
+          type: "button",
+          style: "link",
+          height: "sm",
+          action:{
+            type: "postback",
+            label: select.title,
+            data: (select.type == 'scenario' && select.scene ) ?
+              String(status.turn + 1) + " " + type + " " + select.id + " " + select.scene :
+              String(status.turn + 1) + " " + type + " " + select.id,
+            displayText: select.title
+          }
+        });
+    });
+  }
+  if( flex.contents.footer.contents.length == 0 ){
+    flex.contents.footer.contents.push({
+      type: "button",
+      style: "link",
+      height: "sm",
+      action:{
+        type: "message",
+        label: "シナリオの最初に戻る",
+        text: "リセット"
+      }
+    });
+    flex.contents.footer.contents.push({
+      type: "button",
+      style: "link",
+      height: "sm",
+      action:{
+        type: "message",
+        label: "最初から始める",
+        text: "リタイア"
+      }
+    });
+  }
+  messages.push(flex);
+
+  if( scene.audio && scene.audio.name ){
+    // 音声ファイルが指定されていた場合
+    // アイテムの所持・非所持確認
+    var condition = check_condition(status.items, scene.audio.have, scene.audio.nothave );
+    if( condition ){
+      var audio_buffer = await load_audio(scene.audio.name + '.m4a');
+      var metadata = await mm.parseBuffer(audio_buffer, "audio/aac")
+      var message = {
+        type: "audio",
+        originalContentUrl: encodeURI(AUDIO_URL_BASE + scene.audio.name + '.m4a'),
+        duration: Math.floor(metadata.format.duration * 1000) 
+      };
+      messages.push(message);
+    }
+  }
+
+  //userIdのステータスをDBに更新
+  console.log(status);
+  update_status(status);
+
+  // メッセージの一括送信
+  console.log(messages);
+  console.log(JSON.stringify(messages));
+  return client.replyMessage(event.replyToken, messages);
+}
+
+// ポストバック受信処理
+app.postback(async (event, client) =>{
+  console.log(event);
+
+  try{
+    // userIdのステータスをDBから取得
+    var status = await load_status(event.source.userId);
+    if( !status ){
+      throw "unknown user";
+    }
+
+    var com_list = event.postback.data.split(' ');
+
+    switch(com_list[1]){
+      case 'scenario':{
+        // シナリオの変更
+        if( parseInt(com_list[0]) != (status.turn + 1) )
+          throw "turn is invalid";
+        status.scenario = com_list[2];
+        status.items = [];
+        if( com_list.length > 3 )
+          status.scene = com_list[3];
+        else
+          status.scene = DEFAULT_SCENE;
+        break;
+      }
+      case 'scene':{
+        // シーンの変更
+        if( parseInt(com_list[0]) != (status.turn + 1))
+          throw "turn is invalid";
+        status.scene = com_list[2];
+        break;
+      }
+      default:{
+        // 不明なコマンド
+        var message = {
+          type: "text",
+          text: "不明なコマンド",
+        };
+        return client.replyMessage(event.replyToken, message);
+      }
+    }
+
+    // 現在のシナリオを取得
+    let scenario = await load_scenario(status.scenario + '.json');
+    if( !scenario )
+      throw "scenario not found";    
+    scenario = JSON.parse(scenario);
+
+    return await process_scenario(event, client, scenario, status);
+  }catch(error){
+    console.error(error);
+    var message = {
+      type: "text",
+      text: error.toString()
+    };
+    return client.replyMessage(event.replyToken, message);
+  }
+});
+
+// メッセージ受信処理
 app.message(async (event, client) =>{
   try{
     console.log(event);
-
-    var cmd_processed = false;
 
     // userIdのステータスをDBから取得
     var status = await load_status(event.source.userId);
@@ -220,105 +460,80 @@ app.message(async (event, client) =>{
       await insert_status(status);
     }
 
-    if( event.message.text == 'ヘルプ'){
-      // コマンド：ヘルプ
-      var message = {
-        type: "text",
-        text: "コマンドリスト\n  リタイア：最初からやり直し\n  リセット：今のシナリオの最初に戻る\n  リロード：再表示\n  持ち物：持ち物の確認\n  ステータス：シナリオ名の確認",
-        quickReply: {
-          items: [
-            {
-              type: 'action',
-              action: {
-                type: "message",
-                label: "リセット",
-                text: "リセット"
-              }
-            },
-            {
-              type: 'action',
-              action: {
-                type: "message",
-                label: "リロード",
-                text: "リロード"
-              }
-            },
-            {
-              type: 'action',
-              action: {
-                type: "message",
-                label: "持ち物",
-                text: "持ち物"
-              }
-            },
-            {
-              type: 'action',
-              action: {
-                type: "message",
-                label: "ステータス",
-                text: "ステータス"
-              }
-            },
-          ]
-        }
-      };
-      return client.replyMessage(event.replyToken, message);
-    }
-
-    var com_list = event.message.text.split(' ');
-
-    switch(com_list[0]){
+    // 現在のシナリオを取得
+    let scenario = await load_scenario(status.scenario + '.json');
+    if( !scenario )
+      throw "scenario not found";    
+    scenario = JSON.parse(scenario);
+    
+    switch(event.message.text){
+      case 'ヘルプ':
+      case '@':{
+        // コマンド：ヘルプ
+        var message = {
+          type: "text",
+          text: "コマンドリスト\n  リタイア：最初からやり直し\n  リセット：今のシナリオの最初に戻る\n  リロード：再表示\n  持ち物：持ち物の確認\n  ステータス：シナリオ名の確認",
+          quickReply: {
+            items: [
+              {
+                type: 'action',
+                action: {
+                  type: "message",
+                  label: "リセット",
+                  text: "リセット"
+                }
+              },
+              {
+                type: 'action',
+                action: {
+                  type: "message",
+                  label: "リロード",
+                  text: "リロード"
+                }
+              },
+              {
+                type: 'action',
+                action: {
+                  type: "message",
+                  label: "持ち物",
+                  text: "持ち物"
+                }
+              },
+              {
+                type: 'action',
+                action: {
+                  type: "message",
+                  label: "ステータス",
+                  text: "ステータス"
+                }
+              },
+            ]
+          }
+        };
+        return client.replyMessage(event.replyToken, message);
+      }
       case 'リタイア':{
         // コマンド：リタイア
         status = await create_status(event.source.userId, DEFAULT_SCENARIO, DEFAULT_SCENE);
-        cmd_processed = true;
-        break;
+        let scenario = await load_scenario(status.scenario + '.json');
+        if( !scenario )
+          throw "scenario not found";
+        scenario = JSON.parse(scenario);
+
+        return process_scenario(event, client, scenario, status);
       }
       case 'リセット':{
         // コマンド：リセット
         status.scene = DEFAULT_SCENE;
         status.items = [];
-        cmd_processed = true;
-        break;
+
+        return process_scenario(event, client, scenario, status);
       }
       case 'リロード':{
         // コマンド：リロード
-        cmd_processed = true;
-        break;
-      }
-      case 'scenario':{
-        // シナリオの変更
-        if( parseInt(com_list[2]) != (status.turn + 1) )
-          throw "turn is invalid";
-        status.scenario = com_list[1];
-        status.scene = DEFAULT_SCENE;
-        cmd_processed = true;
-        break;
-      }
-      case 'scene':{
-        // シーンの変更
-        if( parseInt(com_list[2]) != (status.turn + 1))
-          throw "turn is invalid";
-        status.scene = com_list[1];
-        cmd_processed = true;
-        break;
+        return process_scenario(event, client, scenario, status);
       }
     }
-
-    // ターン番号をインクリメント
-    status.turn++;
-
-    // 現在のシナリオを取得
-    let scenario = await load_scenario(status.scenario + '.json');
-    if( !scenario )
-      throw "scenario not found";
-    
-    scenario = JSON.parse(scenario);
-
-    // 現在のシーンを取得
-    var scene = scenario.scene.find(item => item.id == status.scene );
-    if( !scene )
-      throw "scene not found";
 
     if( event.message.text == '持ち物' || event.message.text == 'もちもの'){
       // コマンド：持ち物
@@ -333,7 +548,6 @@ app.message(async (event, client) =>{
       }else{
         message.text += "はありません";
       }
-      cmd_processed = true;
       return client.replyMessage(event.replyToken, message);
     }else
     if( event.message.text == 'ステータス'){
@@ -342,186 +556,15 @@ app.message(async (event, client) =>{
         type: "text",
         text: "シナリオ名：" + scenario.title + "\nシーン番号：" + status.scene,
       };
-      cmd_processed = true;
       return client.replyMessage(event.replyToken, message);
-    }
-    
-    var messages = [];
-
-    if(!cmd_processed ){
+    }else{
       // 不明なコマンド
       var message = {
         type: "text",
         text: "不明なコマンド",
       };
-      messages.push(message);
+      return client.replyMessage(event.replyToken, message);
     }
-
-    // 獲得アイテムの処理
-    if(scene.acquire && scene.acquire.length > 0){
-      scene.acquire.forEach(item => add_item(status.items, item));
-      var message = {
-        type: "text",
-        text: scene.acquire.join('、') + "を手に入れた"
-      }
-      messages.push(message);
-    }
-
-    // ロストアイテムの処理
-    if( scene.lost && scene.lost.length > 0){
-      scene.lost.forEach(item => remove_item(status.items, item));
-      var message = {
-        type: "text",
-        text: scene.lost.join('、') + "を失った"
-      };
-      messages.push(message);
-    }
-
-    // メインダイアログ
-    var flex = {
-      type: "flex",
-      altText: scene.text,
-      contents: {
-        type: "bubble",
-        size: "kilo",
-        body: {
-          type: "box",
-          layout: "vertical",
-          contents: [],
-        },
-        footer:{
-          type: "box",
-          layout: "vertical",
-          contents:[],
-          flex: 0
-        }
-      }
-    };
-
-    if( scene.image && scene.image.background ){
-      // 画像が指定されていた場合
-      var image_url = IMAGE_URL_BASE + scene.image.background;
-      // 画像合成が指定されていた場合
-      if(scene.image.composite ){
-        scene.image.composite.forEach( select => {
-          if( !select.name )
-            return;
-
-          // アイテムの所持・非所持確認
-          var condition = check_condition(status.items, select.have, select.nothave );
-          if( !condition )
-            return;
-
-          // 合成画像の指定追加
-          image_url += '-' + select.name;
-          if( select.position != undefined )
-            image_url += '_' + select.position;
-        });
-      }
-      flex.contents.hero = {
-        type: "image",
-        url: encodeURI(image_url),
-        size: "full",
-        aspectRatio: "20:13",
-        aspectMode: "fit"
-      };
-    }
-
-    if( scene.title ){
-      // タイトルが指定されていた場合
-      flex.contents.body.contents.push({
-        type: "text",
-        wrap: true,
-        text: scene.title,
-        weight: "bold",
-        size: "md"
-      });
-    }
-
-    // テキストの設定
-    flex.contents.body.contents.push({
-      type: "text",
-      wrap: true,
-      size: "sm",
-      text: scene.text
-    });
-
-    // 次の選択肢
-    if( scene.selection ){
-      scene.selection.forEach( select =>{
-        if( !select.id )
-          return;
-          
-        // アイテムの所持・非所持確認
-        var condition = check_condition(status.items, select.have, select.nothave );
-        if( !condition )
-          return;
-
-        // 選択肢の追加
-        var type = "scene";
-        if( select.type )
-          type = select.type;
-
-        flex.contents.footer.contents.push({
-          type: "button",
-          style: "link",
-          height: "sm",
-          action:{
-            type: "message",
-            label: select.title + '(' + select.id + ')',
-            text: type + " " + select.id + " " + (status.turn + 1)
-          }
-        })
-      });
-    }
-    if( flex.contents.footer.contents.length == 0 ){
-      flex.contents.footer.contents.push({
-        type: "button",
-        style: "link",
-        height: "sm",
-        action:{
-          type: "message",
-          label: "シナリオの最初に戻る",
-          text: "リセット"
-        }
-      });
-      flex.contents.footer.contents.push({
-        type: "button",
-        style: "link",
-        height: "sm",
-        action:{
-          type: "message",
-          label: "最初から始める",
-          text: "リタイア"
-        }
-      });
-    }
-    messages.push(flex);
-
-    if( scene.audio && scene.audio.name ){
-      // 音声ファイルが指定されていた場合
-      // アイテムの所持・非所持確認
-      var condition = check_condition(status.items, scene.audio.have, scene.audio.nothave );
-      if( condition ){
-        var audio_buffer = await load_audio(scene.audio.name + '.m4a');
-        var metadata = await mm.parseBuffer(audio_buffer, "audio/aac")
-        var message = {
-          type: "audio",
-          originalContentUrl: encodeURI(AUDIO_URL_BASE + scene.audio.name + '.m4a'),
-          duration: Math.floor(metadata.format.duration * 1000) 
-        };
-        messages.push(message);
-      }
-    }
-
-    //userIdのステータスをDBに更新
-    console.log(status);
-    update_status(status);
-
-    // メッセージの一括送信
-    console.log(messages);
-    console.log(JSON.stringify(messages));
-    return client.replyMessage(event.replyToken, messages);
   }catch(error){
     console.error(error);
     var message = {
